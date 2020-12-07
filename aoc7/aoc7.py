41c776aa44afc65862964fc853a2d90d070e53a5
Bruno Oliveira <olivbruno8@gmail.com>
2020-12-07 22:46:25 +0100

    Update aoc7.py

--- aoc7/aoc7.py
+++ aoc7/aoc7.py
@@ -17,28 +17,18 @@ def solve(l, colors, ans):
         ans.append(clr)
     return solve(l,set(colorsRecurse), ans)
 
-#part 2 we need different processing, top-down
+#part 2 we need different processing, bottom-up
 def solve2(l, colors, ans):
-    tmp = l
-    fstlevel=[]
-    print "Colors are: ",colors
-    for c in set(colors):
-        print "color to filter:a",len(c)
-        for x in tmp:
-            if c in x.split("contain")[1]:
-                fstlevel.append(x)
-    print "fstlevel ",set(fstlevel)
-    if fstlevel==[]:
-        return ans
-    colorsRecurse = []
-    for e in fstlevel:
-        colorsRecurse.append(e.split("contain")[0].strip())
-    for clr in set(colorsRecurse):
-        ans.append(clr)
-    return solve(l,set(colorsRecurse), ans)
+    base = map(lambda x:x.split("contain")[0].strip(), filter(lambda x:"no other" in x,l))
+    x=map(lambda x:x.split("contain")[1].strip(), l)
+    for elem in x:
+        z=map(str.strip , elem.split(","))
+        for ii in z:
+            if ii in base:
+                
 
 
-with open("sampleinput2.txt") as f:
+with open("input.txt") as f:
     l=map(str.strip, f.readlines())
     l=map(lambda s: str.replace(s,"bags",""),l)
     l=map(lambda s: str.replace(s,"bag",""),l)
